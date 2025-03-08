e771ef4d76475c440d93ae69213f310057e72790
janr0599 <javiernr0599@gmail.com>
2025-03-08 09:33:05 -0400

    add: layouts folder and authlayout for login and register views

--- src/views/RegisterView.tsx
+++ src/views/RegisterView.tsx
@@ -3,8 +3,6 @@ import { Link } from "react-router-dom";
 function RegisterView() {
     return (
         <>
-            <div className="text-3xl font-bold">RegisterView</div>
-
             <nav className="hover:opacity-80 transition-opacity">
                 <Link to="/auth/login">Ya tienes cuenta? Inicia Sesión</Link>
             </nav>
